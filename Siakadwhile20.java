0c67def43ec43a899bd9d6aa12a8e7129a21ae34
daebon07 <muhammadshabran25@gmail.com>
2024-10-10 15:32:39 +0700

    percobaan 2

--- Siakadwhile20.java
+++ Siakadwhile20.java
@@ -9,15 +9,36 @@ public class Siakadwhile20 {
         jml = sc.nextInt();
 
         while ( i < jml) {
-            System.out.println("masukan nilai mahasiswa ke" + (i+1) + ":");
+            System.out.print("masukan nilai mahasiswa ke " + (i+1) + " :");
             nilai = sc.nextInt();
-            i++;
-
-
+            
             if (nilai < 0 || nilai > 100){
                 System.out.println( "nilai tidak valid masukan nilai yang valid ");
+                continue;
+            }
+            if (nilai > 80 && nilai <= 100){
+                System.out.println("nilai mahasiswa" + (i+1) + "adalah a");
+            }
+            else if (nilai > 73 && nilai <= 80){
+                System.out.println("nilai mahasiswa" + (i+1) + "adalah b+");
+            }
+            else if (nilai > 65 && nilai <= 73){
+                System.out.println("nilai mahasiswa" + (i+1) + "adalah b");
+            }
+            else if (nilai > 60 && nilai <= 65){
+                System.out.println("nilai mahasiswa" + (i+1) + "adalah c+");
+            }
+            else if (nilai > 50 && nilai <= 60){
+                System.out.println("nilai mahasiswa" + (i+1) + "adalah c");
+            }
+            else if (nilai > 39 && nilai <=50){
+                System.out.println("nilai mahasiswa" + (i+1) + "adalah d");
             }
             
+            else {
+                System.out.println("nilai mahasiswa" + (i+1) + "adalah e");
+            }
+            i++;
         }
         
     }
