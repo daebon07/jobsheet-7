6cf02900b325c059fc57d896bc513ff64ce88101
daebon07 <muhammadshabran25@gmail.com>
2024-10-10 18:39:04 +0700

    jawaban pertanyaan 1 nomor 4&5

--- SiakadFor20.java
+++ SiakadFor20.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class SiakadFor20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+ 
+        int lulus = 0, tidaklulus = 0;
         double nilai, tertinggi = 0, terendah = 100;
 
         for (int i = 1; i <= 10; i++){
@@ -14,8 +15,17 @@ public class SiakadFor20 {
             if (nilai < terendah){
                 terendah = nilai;
             }
+            if (nilai >= 60){
+                lulus +=1;
+            }
+            if (nilai <= 59){
+                tidaklulus +=1;
+            }
         }
+        
         System.out.println("nilai tertinggi :" + tertinggi);
         System.out.println("nilai terendah :" + terendah);
+        System.out.println("jumlah mahassiswa yang lulus :" + lulus);
+        System.out.println("nilai jumlah mahasiswa yang tidak lulus :" + tidaklulus);
     }
 }
